6fa79b43be7709094a4aeea05a95890b12bccb15
sigizmund <sigizmund@0279230e-09aa-11df-830e-35ef22166b74>
2010-01-25 12:09:29 +0000

    Initial import

---
+++ model.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+model.py
+
+Created by Roman Kirillov on 2010-01-25.
+"""
+
+import datetime
+from google.appengine.ext import db
+from google.appengine.api import users
+
+
+class Visitor(db.Model):
+    '''
+    Simple object which uses Google DataStore persistence model
+    '''
+    
+    '''UserProperty encapsulates information about a Google's 
+    user account available to application. It is set to
+    use current user as a default user automatically
+    whever new object is created'''
+    user = db.UserProperty(required = True, auto_current_user_add = True) 
+    
+    '''DateTimeProperty uses standard underlying datetime.datetime
+    and is set to auto-update to now() whenever record is saved'''
+    lastVisit = db.DateTimeProperty(auto_now = True)
+    
+    '''Very simple integer property with default value of 0'''
+    hits = db.IntegerProperty(default = 0)
---
+++ view.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+view.py
+
+Created by Roman Kirillov on 2010-01-25.
+Copyright (c) 2010 Yahoo! Inc. All rights reserved.
+"""
+
+import os
+from google.appengine.api import users
+from google.appengine.ext.webapp import template
+
+class StartPage:
+    def __init__(self, request):
+        self.request = request
+    
+    def render(self, out):
+        template_values = {
+            'login_url' : users.create_login_url("/login")
+        }
+        
+        path = os.path.join('templates/please_login.html')
+        out.write(template.render(path, template_values))
+
+class WelcomePage:
+    def __init__(self, request):
+        self.request = request
+        self.user = users.get_current_user()
+
+    def render(self, out):
+        template_values = {
+            'username' : self.user.nickname(),
+            'logout_url' : users.create_logout_url('/')
+        }
+
+        path = os.path.join('templates/welcome.html')
+        out.write(template.render(path, template_values))
+        
+class StatsPage:
+    def __init__(self, request, visitor):
+        self.request = request
+        self.visitor = visitor
+
+    def render(self, out):
+        
+        template_values = {
+            'username' : self.visitor.user,
+            'lastVisit' : self.visitor.lastVisit,
+            'hits' : self.visitor.hits
+        }
+
+        path = os.path.join('templates/stats.html')
+        out.write(template.render(path, template_values))    
